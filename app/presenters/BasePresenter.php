183f29da8f09e17b22802ea9e7ca7e1962696bd7
Petr Babicka <babcca@gmail.com>
2012-02-05 00:15:25 +0100

    Upraveni zakladni aplikace

--- app/presenters/BasePresenter.php
+++ app/presenters/BasePresenter.php
@@ -11,24 +11,12 @@ require_once LIBS_DIR.'/Facebook/facebook.php';
 
 abstract class BasePresenter extends Nette\Application\UI\Presenter
 {
-  protected $facebook;
-  protected function getModel($model) {
-    return $this->context->modelLoader->getModel($model);
-  }
-  
-  protected function startup() {
-    parent::startup();
-    /* Prepsat cele user auth :( rozmyslet jak bude vypadat prihlasovaci politika */
-    $this->facebook = new Facebook(array(
-      "appId"=>"106523352701146",
-      "secret"=>"3943380c0e7ac42c48c83592cec8b2b8"));
-      
-	  $user = $this->facebook->getUser(); // ma byt v user auth 
-	  if (!$user) {
-      $this->template->loged_in = true;
+    /* @var Model */
+    protected $model;
+    
+    protected function startup() {
+        parent::startup();
+        // initialize model service
+        $this->model = $this->getService("model"); 
     }
-    else {
-      $this->template->loged_in = false;
-    }
-  }
-}
+ }
--- app/presenters/HomepagePresenter.php
+++ app/presenters/HomepagePresenter.php
@@ -8,50 +8,13 @@
  */
 
 use Nette\Application\UI,
-	Nette\Diagnostics\Debugger;
+        Nette\Diagnostics\Debugger;
 
-class HomepagePresenter extends BasePresenter
+final class HomepagePresenter extends BasePresenter
 {
 
-	public function renderDefault()
-	{
-    $this->template->records = $this->getModel("record")->GetAllRecords();
-    //$this->invalidateControl('ahoj');
-    $this->template->results = "ahojky";
-    
-	}
-	
-	protected function createComponentSearch() {
-        $search = new SearchControl;
-        //$search->addComponent(new ResultControl, "lkl_result");
-        $form = new UI\Form;
-        $form->addComponent($search, "lkl_search");
-        $form->addSubmit('save', 'Hledat');
-        $form->onSuccess[] = callback($this, "searchingSubmitted");
-        //$this->addComponent($search, "search");
-        return $form;
-     //$search = new SearchControl;
-	 //$search->invalidateControl();
-	 //return $search;
-  }
-  protected function createComponentResult() {
-        $result = new ResultControl;
-        $result->invalidateControl();
-        $this->addComponent($result, "result");
-        
-     //$search = new SearchControl;
-	 //$search->invalidateControl();
-	 //return $search;
-  }
-  
-  public function searchingSubmitted($form) {
-        $values = $form->getValues();
-        $this->template->results = $values["search_what"];
-        //$form->invalidateControl('show');
-        $form->getComponents();
-        $form->getPresenter()->invalidateControl($form->getName());
-        //  Debugger::dump($form->getComponents()->lkl_search);
-        Debugger::dump($values);
-    
-    }
+    public function renderDefault()
+    {
+        $this->template->results = "ahojky";
+    }	
 }
